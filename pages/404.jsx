d7819eee9af84d295baad38eb67566fdf7e12ceb
vipscodes <vipscodes@gmail.com>
2021-07-23 12:55:31 +0700

    Custom 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Oops</h1>
+      <p className="title-not-found">Halaman yang anda c ari tidak ditemukan</p>
+    </div>
+  );
+}
