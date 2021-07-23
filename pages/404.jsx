9b0b4c4b58a4fb85ca970cda6637d96ec88f8df3
vipscodes <vipscodes@gmail.com>
2021-07-23 13:00:25 +0700

    Auto Redirection

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 5000);
+  }, []);
   return (
     <div>
       <h1 className="title-not-found">Oops</h1>
-      <p className="title-not-found">Halaman yang anda c ari tidak ditemukan</p>
+      <p className="title-not-found">Halaman yang anda cari tidak ditemukan</p>
     </div>
   );
 }
