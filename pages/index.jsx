bfd5f5f9c406f8e0bd106fba4ab3a8f17478b005
vipscodes <vipscodes@gmail.com>
2021-07-23 08:26:45 +0700

    Component Link

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Home() {
+  
+  return (
+    <>
+      <header>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/blog">Blog</Link></li>
+          <li><Link href="/users">Users</Link></li>
+        </ul>
+      </header>
+      <h1>Welcome Avip</h1>
+    </>
+  )
+}
