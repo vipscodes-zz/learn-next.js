e2ed1cfe8d9b8f8411393a50630fefab09a763fa
vipscodes <vipscodes@gmail.com>
2021-07-23 13:25:06 +0700

    Optimize Image

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,10 @@
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
-import Head from "next/head";
+import Image from "next/image";
 export default function Home() {
   return (
     <Layout titlePage="Home">
+      <Image src="/vercel.svg" width={300} height={200} alt="profile" />
       <h1 className={styles["title-homepage"]}>Welcome Avip</h1>
     </Layout>
   );
