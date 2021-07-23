9f297b280f853047b4aa302920ff02993d4461ec
vipscodes <vipscodes@gmail.com>
2021-07-23 13:28:01 +0700

    Dynamic Routing

---
+++ pages/users/[id].jsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+export default function UserDetail() {
+  const router = useRouter();
+  const { id } = router.query;
+  return (
+    <div>
+      <p>User Detail Page {id}</p>
+    </div>
+  );
+}
